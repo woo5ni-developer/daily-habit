91612e602c18a9393e6a560075029480bdcdd078
woowoni <ewoowon.2@gmail.com>
2023-01-02 20:49:34 +0900

    컬러선택 컴포넌트 파일생성

--- src/routes/set.tsx
+++ src/routes/set.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react'
+import ButtonColorField from '../components/ButtonColorField'
 
 const Set: FC = () => {
-  return <div className="text-red-600">Set</div>
+  return (
+    <div className="text-red-600">
+      Set
+      <ButtonColorField />
+    </div>
+  )
 }
 
 export default Set
