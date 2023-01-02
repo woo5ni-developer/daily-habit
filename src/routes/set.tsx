73f73657bcfb511946bb606e2535a865f16eee33
sdong4884 <sdong4884@gmail.com>
2023-01-02 20:45:24 +0900

    add HabitTerm Components

--- src/routes/set.tsx
+++ src/routes/set.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react'
+import HabitTerm from '../components/HabitTerm'
 
 const Set: FC = () => {
-  return <div className="text-red-600">Set</div>
+  return (
+    <div className="text-red-600">Set
+      <HabitTerm />
+    </div>
+  )
 }
 
 export default Set
