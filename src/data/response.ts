21dff99a0b123c823c64a26e903c04df7f55bb86
woowoni <ewoowon.2@gmail.com>
2023-02-18 23:59:31 +0900

    color data type 정의

--- src/data/response.ts
+++ src/data/response.ts
@@ -1,4 +1,4 @@
-import { NavType } from '../lib/type'
+import { ColorType, NavType } from '../lib/type'
 
 export const navList: NavType[] = [
   { id: 1, title: 'Home', link: '/' },
@@ -6,4 +6,31 @@ export const navList: NavType[] = [
   { id: 3, title: 'Tracker', link: '/tracker' },
   { id: 4, title: 'Stats', link: '/stats' },
   { id: 5, title: 'Setting', link: '/setting' },
+]
+
+export const colorList: ColorType[] = [
+  {
+    id: 1,
+    color: '#845EC2',
+  },
+  {
+    id: 2,
+    color: '#D65DB1',
+  },
+  {
+    id: 3,
+    color: '#FF6F91',
+  },
+  {
+    id: 4,
+    color: '#FF9671',
+  },
+  {
+    id: 5,
+    color: '#FFC75F',
+  },
+  {
+    id: 6,
+    color: '#F9F871',
+  },
 ]
