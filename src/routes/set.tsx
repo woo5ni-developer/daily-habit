485158d129a79a49ebe5028a428354ef3110596a
woowoni <ewoowon.2@gmail.com>
2023-02-27 22:40:22 +0900

    button color Field eventhandle

--- src/routes/set.tsx
+++ src/routes/set.tsx
@@ -2,12 +2,16 @@ import React, { FC } from 'react'
 import ButtonColorField from '../components/ButtonColorField'
 import HabitTerm from '../components/HabitTerm'
 import { colorList } from '../data/response'
+import { ColorType } from '../lib/type'
 
 const Set: FC = () => {
+  const SelectColor = (color: ColorType): void => {
+    console.log(color.color)
+  }
   return (
     <div className="text-red-600">
       Set
-      <ButtonColorField data={colorList} />
+      <ButtonColorField data={colorList} handleClick={SelectColor} />
       <HabitTerm />
     </div>
   )
