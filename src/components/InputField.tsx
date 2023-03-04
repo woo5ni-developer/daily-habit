7a082659c1a37e99dd25b26b83ab5674c4c4bc39
munsoyeon <munsy0111@gmail.com>
2023-03-04 18:54:25 +0900

    인풋 입력한 값 담길 변수 추가

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,16 +1,22 @@
 // import { border } from '@mui/system'
 import React, { FC } from 'react'
 import Label from '../components/Label'
+import { useState } from 'react'
 
 const InputField: FC = () => {
+  const [inputValue, setInputValue] = useState('')
   return (
     <div>
       <Label text="습관이름" labelName="name" />
       <input
         type="text"
-        className="rounded-[10px] w-full h-[40px] bg-[#D9D9D9] opacity-20 text-[#b6b6b6] px-[16px] mt-[10px]"
+        className="rounded-[10px] w-full h-[40px] bg-[#f7f7f7] text-[#b6b6b6] px-[16px] mt-[10px]"
         placeholder="습관이름을 입력하세요."
         id="name"
+        onChange={(event) => {
+          setInputValue(event.target.value)
+          console.log(inputValue)
+        }}
       />
     </div>
   )
