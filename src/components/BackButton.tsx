31e9f9277bc9c489e127f62f204ac251c778aa8d
munsoyeon <munsy0111@gmail.com>
2023-03-16 17:31:56 +0900

    스타일링중

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -3,16 +3,17 @@ import { useNavigate } from 'react-router-dom'
 
 const BackButton: FC = () => {
   const navigate = useNavigate()
-  const goBack = () => {
+  const goBack = (): void => {
     navigate(-1)
   }
   return (
-    <div>
-      <button className="absolute px-[24px] py-[35px] top-0 left-0" onClick={goBack}>
-        &#60;
-      </button>
-    </div>
+    <button
+      className="absolute px-[24px] py-[35px] top-0 left-0 w-[11px] h-[18px] block"
+      onClick={goBack}>
+      <span className="overflow-hidden inline-block relative z-[-1] border-0 w-px h-px">
+        뒤로가기
+      </span>
+    </button>
   )
 }
-
 export default BackButton
