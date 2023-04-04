2abc755d537e85489fa1653cc026373cf5f9cd67
woowoni <ewoowon.2@gmail.com>
2023-04-04 22:25:49 +0900

    home 메인 화면 작업

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react'
 
 const Home: FC = () => {
-  return <div className="text-red-600">Home</div>
+  return (
+    <div>
+      Today Habit
+      <img src="./images/bg/bg-home-habit.png" alt="" />
+    </div>
+  )
 }
 
 export default Home
