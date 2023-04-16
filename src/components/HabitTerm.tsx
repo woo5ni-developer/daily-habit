fce78e819ec45612a53986644232cbd851c679a8
munsoyeon <munsy0111@gmail.com>
2023-04-16 11:29:23 +0900

    스타일링 등 작업중

--- src/components/HabitTerm.tsx
+++ src/components/HabitTerm.tsx
@@ -1,7 +1,38 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import Label from '../components/Label'
 
 const HabitTerm: FC = () => {
-  return <div>HabitTerm</div>
+  const [startValue, setStartValue] = useState('')
+  const [endValue, setEndValue] = useState('')
+  return (
+    <div>
+      <Label text="당신은 얼마나 오래 이 습관을 유지할건가요?" labelName="termStart" />
+      <div className="border-[1px] inline-block relative">
+        <label htmlFor="termStart" className="absolute bg-[#ffffff] text-[12px]">
+          start
+        </label>
+        <input
+          type="date"
+          id="termStart"
+          className="h-[42px]"
+          value={startValue}
+          onChange={(event) => setStartValue(event.target.value)}
+        />
+      </div>
+      <div className="border-[1px] inline-block relative ml-[10px]">
+        <label htmlFor="termEnd" className="absolute bg-[#ffffff] text-[12px]">
+          End
+        </label>
+        <input
+          type="date"
+          id="termEnd"
+          className="h-[42px]"
+          value={endValue}
+          onChange={(event) => setEndValue(event.target.value)}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default HabitTerm
