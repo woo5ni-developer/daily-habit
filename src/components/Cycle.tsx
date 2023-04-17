b52755f7b1bee40e114ae446af91ce5512e33833
munsoyeon <munsy0111@gmail.com>
2023-04-17 19:40:36 +0900

    코드에서 styled-component 제거하고 tailwind peer로 대체

--- src/components/Cycle.tsx
+++ src/components/Cycle.tsx
@@ -2,7 +2,6 @@
 import React, { FC, useState } from 'react'
 import { RadioType } from '../lib/type'
 import Label from '../components/Label'
-import styled from 'styled-components'
 
 interface CycleProps {
   data: RadioType[]
@@ -11,15 +10,6 @@ interface CycleProps {
 }
 
 const Cycle: FC<CycleProps> = ({ data, radioHandler, SelectColor }) => {
-  const RadioLabel = styled.label``
-  const RadioInput = styled.input`
-    &:checked + ${RadioLabel} {
-      background-color: ${SelectColor.color};
-    }
-    &:focus + ${RadioLabel} {
-      outline: 1px solid #ffffff;
-    }
-  `
   const [selectValue, setSelectValue] = useState('Morning')
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -31,23 +21,23 @@ const Cycle: FC<CycleProps> = ({ data, radioHandler, SelectColor }) => {
       <Label text="당신은  하루 중 언제 이 습관을 하는 게 좋은가요?" labelName="term" />
       {data.map((data) => (
         <div key={data.id} className="inline-flex relative m-[5px] radio-button">
-          <RadioInput
+          <input
             type="radio"
             id={'radio' + data.id}
-            className="opacity-[0] absolute inset-0"
+            className="opacity-[0] absolute inset-0 peer"
             name="term"
             value={data.text}
             onChange={onChange}
           />
-          <RadioLabel
+          <label
             style={{
               backgroundImage: `url(${data.path})`,
               backgroundSize: data.bgWidth,
             }}
-            className="rounded-[10px] px-[12px] py-[8px] pl-[35px] bg-no-repeat bg-[left_12px_center] bg-[#d9d9d9]"
+            className="rounded-[10px] px-[12px] py-[8px] pl-[35px] bg-no-repeat bg-[left_12px_center] bg-[#d9d9d9] peer-checked:bg-[#ffffff]"
             htmlFor={'radio' + data.id}>
             {data.text}
-          </RadioLabel>
+          </label>
         </div>
       ))}
     </div>
