3a098474461dfc83cf0aa456285f24c301003af1
munsoyeon <munsy0111@gmail.com>
2023-04-17 21:36:18 +0900

    부모에게 값 전달

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,8 +3,17 @@ import React, { FC } from 'react'
 import Label from '../components/Label'
 import { useState } from 'react'
 
-const InputField: FC = () => {
+interface InputFieldProps {
+  onChange: (value: string) => void
+}
+
+const InputField: FC<InputFieldProps> = ({ onChange }) => {
   const [inputValue, setInputValue] = useState('')
+
+  const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value)
+  }
+
   return (
     <div>
       <Label text="습관이름" labelName="name" />
@@ -13,10 +22,7 @@ const InputField: FC = () => {
         className="rounded-[10px] w-full h-[40px] bg-[#f7f7f7] text-[#b6b6b6] px-[16px] mt-[10px]"
         placeholder="습관이름을 입력하세요."
         id="name"
-        onChange={(event) => {
-          setInputValue(event.target.value)
-          console.log(inputValue)
-        }}
+        onChange={inputChange}
       />
     </div>
   )
