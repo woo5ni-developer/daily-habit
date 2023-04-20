b424469686ec2d96cd1fbbe767f309bad24d9b7d
munsoyeon <munsy0111@gmail.com>
2023-04-20 11:28:06 +0900

    x-date-pickers-pro, dayjs 추가 / 추가된 컴포넌트 활용 ui 정리

--- src/components/HabitTerm.tsx
+++ src/components/HabitTerm.tsx
@@ -1,36 +1,23 @@
 import React, { FC, useState } from 'react'
+import dayjs from 'dayjs'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import { LocalizationProvider } from '@mui/x-date-pickers-pro'
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs'
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker'
 import Label from '../components/Label'
 
 const HabitTerm: FC = () => {
-  const [startValue, setStartValue] = useState('')
-  const [endValue, setEndValue] = useState('')
   return (
     <div>
-      <Label text="당신은 얼마나 오래 이 습관을 유지할건가요?" labelName="termStart" />
-      <div className="border-[1px] inline-block relative">
-        <label htmlFor="termStart" className="absolute bg-[#ffffff] text-[12px]">
-          start
-        </label>
-        <input
-          type="date"
-          id="termStart"
-          className="h-[42px]"
-          value={startValue}
-          onChange={(event) => setStartValue(event.target.value)}
-        />
-      </div>
-      <div className="border-[1px] inline-block relative ml-[10px]">
-        <label htmlFor="termEnd" className="absolute bg-[#ffffff] text-[12px]">
-          End
-        </label>
-        <input
-          type="date"
-          id="termEnd"
-          className="h-[42px]"
-          value={endValue}
-          onChange={(event) => setEndValue(event.target.value)}
-        />
-      </div>
+      <Label text="당신은 얼마나 오래 이 습관을 유지할건가요?" labelName="period" />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={['DateRangePicker']}>
+          <DateRangePicker
+            localeText={{ start: 'Start', end: 'End' }}
+            defaultValue={[dayjs('2022-04-17'), dayjs('2022-04-21')]}
+          />
+        </DemoContainer>
+      </LocalizationProvider>
     </div>
   )
 }
