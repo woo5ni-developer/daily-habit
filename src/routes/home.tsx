bebf79c153ac2cdc0489b5d58a70df72ab0d592d
woowoni <ewoowon.2@gmail.com>
2023-05-31 23:00:39 +0900

    home 리스트, recoil-persist 적용중

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,29 +3,27 @@ import { IconContext } from 'react-icons'
 import { useRecoilValue } from 'recoil'
 import { habitListState } from '../state/dataState'
 import SelectDay from '../components/SelectDay'
-import { BiCycling } from 'react-icons/bi'
 
 const Home: FC = () => {
-  const habit = [useRecoilValue(habitListState)]
+  const habitList = useRecoilValue(habitListState)
 
-  console.log(habit)
   return (
     <div>
       <SelectDay />
       {/* habit 등록 */}
 
       {/* 리스트 */}
-      {habit ? (
+      {habitList.length ? (
         <ul>
-          {habit.map((item, idx) => (
+          {habitList.map((item, idx) => (
             <li
               key={idx}
               className="pt-[17px] pb-[17px] pl-[22px] pr-[22px] shadow-[0_2px_11px_3px_rgba(0,0,0,0.06)] rounded-[12px] flex items-center"
-              style={{ background: item[idx].color?.color ?? '' }}>
+              style={{ background: item.color?.color ?? '' }}>
               <IconContext.Provider value={{ size: '30px', style: { padding: '3px' } }}>
-                {item[idx].icon && React.createElement(BiCycling)}
+                {item.icon ? React.createElement(item.icon.icon) : null}
               </IconContext.Provider>
-              <p>{item[idx].title ?? ''}</p>
+              <p>{item.title ?? ''}</p>
             </li>
           ))}
         </ul>
--- src/state/dataState.ts
+++ src/state/dataState.ts
@@ -1,6 +1,9 @@
 import { atom } from 'recoil'
 import { habitItemType } from '../lib/type'
 import { initialHabitList } from './initialState'
+import { recoilPersist } from 'recoil-persist'
+
+const { persistAtom } = recoilPersist()
 
 export const nameState = atom<string>({
   key: 'nameState',
@@ -10,9 +13,10 @@ export const nameState = atom<string>({
 export const habitListState = atom<habitItemType[]>({
   key: 'habitListState',
   default: initialHabitList,
+  // effects_UNSTABLE: [persistAtom],
 })
 
 export const dateFormatState = atom<string>({
   key: 'dateFormatState',
-  default: 'YYYY-MM-DD'
+  default: 'YYYY-MM-DD',
 })
